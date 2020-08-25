55bcaba5dd600eae9bc21261b9a4f71f0dc898af
Mitchel Horvath <mhorvath2@huskers.unl.edu>
2020-08-25 02:44:51 +0000

    added my name and major

--- hello.c
+++ hello.c
@@ -10,5 +10,8 @@
 int main(int argc, char **argv) {
    printf("Mitch Anderson\n");
    printf("Computer Science\n");
+   printf("Mitchel Horvath\n");
+   printf("Mechanical Engineering\n");
+   
    return 0;
 }
