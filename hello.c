db57a828f029bd1c38fc1a0039f9d5c86cd80e4e
manderson135gh <manderson135@huskers.unl.edu>
2020-08-24 21:21:30 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,3 +1,10 @@
+/**
+  *Author: Mitch Anderson
+  *Date: 2020/08/24
+  *
+  *A simple hello world program
+  */
+
 int main(int argc, char **argv) {
    printf("Mitch Anderson\n");
    printf("Computer Science\n");
